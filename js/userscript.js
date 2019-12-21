61efd8b01f5b5cc25ce97624bc0e3a12d7e5645f
Michiel DV <michieldevrieze1@hotmail.com>
2019-12-21 21:47:19 +0100

    push

--- js/userscript.js
+++ js/userscript.js
@@ -181,6 +181,24 @@ $(document).ready(function() {
                 $(this).children('.hidden').fadeOut(500);
             }
         });
+
+        var $tempblock = $(".block_weather");
+
+        $tempblock.mouseenter(function () {
+                $(this).children('h1').animate({
+                    top: '20%',
+                    opacity: '0.5'
+                });
+                $(this).children('.hidden').fadeIn(400);
+        });
+
+        $tempblock.mouseleave(function () {
+                $(this).children('h1').animate({
+                    top: '40%',
+                    opacity: '1.0'
+                });
+                $(this).children('.hidden').fadeOut(500);
+        });
     });
 });
 
